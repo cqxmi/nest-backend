9cf161d9dfc2d89c8d54d6f3b150df9ba4810295
cqx <1175902886@qq.com>
2025-07-14 17:38:15 +0800

    修改结构

--- src/modules/texts/controller.ts
+++ src/modules/texts/controller.ts
@@ -1,13 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { TextsService } from './service';
 import { Text } from './entity';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiTags, ApiResponse } from '@nestjs/swagger';
 
 @ApiTags('文本') // Swagger 分组名称
 @Controller('texts')
 export class TextsController {
   constructor(private readonly textService: TextsService) {}
   @Get('/list')
+  @ApiResponse({
+    status: 200,
+    type: [Text],
+  })
   async findAll(): Promise<Text[]> {
     return await this.textService.findAll();
   }
--- src/modules/users/controller.ts
+++ src/modules/users/controller.ts
@@ -2,13 +2,18 @@ import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UsersService } from './service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entity';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiTags, ApiResponse } from '@nestjs/swagger';
 
 @ApiTags('用户') // Swagger 分组名称
 @Controller('users')
 export class UsersController {
   constructor(private readonly userService: UsersService) {}
+
   @Get('/list')
+  @ApiResponse({
+    status: 200,
+    type: [User], // 明确声明返回User数组
+  })
   async findAll(): Promise<User[]> {
     return await this.userService.findAll();
   }
