2ea21e2dcb37c4e42cd5f7ac0d0eb31c2390126e
cqx <1175902886@qq.com>
2025-07-25 14:55:18 +0800

    增加登录、注册接口

--- src/modules/auth/controller.ts
+++ src/modules/auth/controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './service';
 import { CreateUserDto } from '../users/dto/create-user.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { User } from '../users/entity';
 
 @ApiTags('鉴权') // Swagger 分组名称
@@ -9,8 +9,15 @@ import { User } from '../users/entity';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @ApiOperation({ summary: '注册', description: '注册一个新用户' })
   @Post('/register')
   async create(@Body() loginDto: CreateUserDto): Promise<User> {
     return await this.authService.register(loginDto);
+  }
+
+  @ApiOperation({ summary: '登录', description: '用户登录' })
+  @Post('/login')
+  async login(@Body() loginDto: CreateUserDto): Promise<User> {
+    return await this.authService.login(loginDto);
   }
 }
--- src/modules/auth/service.ts
+++ src/modules/auth/service.ts
@@ -7,6 +7,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from '../users/entity';
 import { CreateUserDto } from '../users/dto/create-user.dto';
+import { comparePassword, hashPassword } from '../../utils/bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -24,18 +25,23 @@ export class AuthService {
     if (existingUser) {
       throw new ConflictException('用户名已存在'); // 返回 409 冲突状态码
     }
-
+    registerArg.password = await hashPassword(registerArg.password);
     return this.authRepository.save(registerArg);
   }
 
   // 登录
-  // async login(loginArg: CreateUserDto): Promise<User> {
-  //   // 先判断有没有
-  //   const existingUser = await this.authRepository.findOne({
-  //     where: { username: loginArg.username }, // 假设 DTO 中有 username 字段
-  //   });
-  //   if (!existingUser) {
-  //     throw new NotFoundException('该用户不存在');
-  //   }
-  // }
+  async login(loginArg: CreateUserDto): Promise<User> {
+    // 先判断有没有
+    const existingUser = await this.authRepository.findOne({
+      where: { username: loginArg.username }, // 假设 DTO 中有 username 字段
+    });
+    if (!existingUser) {
+      throw new NotFoundException('该用户不存在');
+    }
+    if (await comparePassword(loginArg.password, existingUser.password)) {
+      return existingUser;
+    } else {
+      throw new NotFoundException('密码错误');
+    }
+  }
 }
